6cf2c382c1069dbd29f1731675371cf83f0c9949
mirko.viroli <mirko.viroli@unibo.it>
2022-10-28 14:39:38 +0200

    to 15

--- slides/13/code/nested_range/UseRange.java
+++
@@ -1,8 +0,0 @@
-public class UseRange{
-    public static void main(String[] s){
-    	for (int i: new Range(5,12)){
-    	    System.out.println(i);
-    	    // 5 6 7 8 9 10 11 12
-    	}
-    }
-}
